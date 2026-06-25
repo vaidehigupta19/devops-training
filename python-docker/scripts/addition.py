326488af8081b2792992d6fcb547cddbb7beeda0
vaidehigupta19 <vaidehigupta1975@gmail.com>
2026-06-25 21:29:31 +0530

    Add Python scripts and Dockerfile

---
+++ python-docker/scripts/addition.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition =", num1 + num2)
---
+++ python-docker/scripts/multiplication.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Multiplication =", num1 * num2)
